51a5fb56dc80142f51c8943fddcb9568c198c7ec
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-01-12 16:38:35 +0100

    add 5 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { CvComponent } from './cv/cv.component';
+import { ListeComponent } from './liste/liste.component';
+import { ItemComponent } from './item/item.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, SecondComponent],
+  declarations: [AppComponent, FirstComponent, SecondComponent, CvComponent, ListeComponent, ItemComponent, DetailsComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
