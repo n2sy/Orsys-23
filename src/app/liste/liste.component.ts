dd35e5cfb2956ace38826ea03b076badbc6c06b1
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-01-13 09:49:12 +0100

    add a model

--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-liste',
   templateUrl: './liste.component.html',
-  styleUrls: ['./liste.component.css']
+  styleUrls: ['./liste.component.css'],
 })
-export class ListeComponent {
-
+export class ListeComponent implements OnInit {
+  @Input() allCandidats: Candidat[];
+  constructor() {}
+  ngOnInit(): void {
+    console.log(this.allCandidats);
+  }
 }
