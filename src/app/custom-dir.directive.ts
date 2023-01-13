570dcd3c6ba0ca5703c06b11bd9260124f548027
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-01-13 16:22:46 +0100

    add custom dir

---
+++ src/app/custom-dir.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomDir]',
+})
+export class CustomDirDirective {
+  @HostBinding('style.backgroundColor') bgColor = 'pink';
+  @HostBinding('style.color') cl = 'blue';
+
+  constructor() {}
+
+  @HostListener('mouseenter') mouseenter() {
+    this.bgColor = 'grey';
+    this.cl = 'white';
+  }
+  @HostListener('mouseleave') mouseleave() {
+    this.bgColor = 'white';
+    this.cl = 'black';
+  }
+}
