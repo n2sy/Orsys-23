903aeebbc33bca8d41909d8aee2e75f5cab0f571
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-01-13 09:34:56 +0100

    add a model

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css']
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent {
-
+  tabCandidats: Candidat[] = [
+    new Candidat(1, 'Nidhal', 'Jelassi', 36, 'Formateur', 'nidhal.jpg'),
+    new Candidat(2, 'Bart', 'Simpson', 26, 'Ingénieur', 'bart.jpeg'),
+    new Candidat(1, 'Homer', 'Simpson', 51, 'Directeur', 'homer.jpg'),
+  ];
 }
