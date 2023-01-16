6999a18a92e93014a68b396a17263e7f6d381883
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-01-16 15:19:41 +0100

    a routerlink and navigate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  goToServers() {
+    this.router.navigateByUrl('/servers'); // route statique
+  }
+
+  goToCv() {
+    //....
+    this.router.navigate(['/cv']); // route dynamique
+  }
 }
